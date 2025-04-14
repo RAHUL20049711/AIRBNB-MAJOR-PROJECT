448afecbfa2fc2ab2df7e02e8ebb331aeb5a4f1c
Rahul20049711 <rs4862517@gmail.com>
2025-04-15 01:45:22 +0530

    addedMVCalongwithmorefeatures

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config()  //only for development not for production
+}
+
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
--- routes/review.js
+++ routes/review.js
@@ -7,35 +7,22 @@ const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
 const {isLoggedIn, isReviewAuthor}= require("../middleware.js");
 
-
+const reviewController= require("../controllers/reviews.js");
 
 
 //Reviews
 //Post Review Route
-router.post("/",isLoggedIn,validateReview, wrapAsync( async(req, res)=>{
-        let listing= await Listing.findById(req.params.id);
-    let newReview= new Review(req.body.review);
-    newReview.author= req.user._id;
-    listing.reviews.push(newReview);
-     await newReview.save();
-     await listing.save();
-     req.flash("success", "New Review Created!");
-     return res.redirect(`/listings/${listing._id}`);
-    
-    
- }));
+router.post("/",
+    isLoggedIn,
+    validateReview, 
+    wrapAsync( reviewController.createReview));
  
  //Delete Review Route
- router.delete("/:reviewId",isLoggedIn,isReviewAuthor, wrapAsync(async(req,res)=>{
-        let{id, reviewId}= req.params;
-        await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-        await Review.findByIdAndDelete(reviewId);
-        req.flash("success", "Review Deleted!");
-        return res.redirect(`/listings/${id}`);
-    
-    
-    
- }));
+ router.delete(
+    "/:reviewId",
+    isLoggedIn,
+    isReviewAuthor, 
+    wrapAsync(reviewController.destroyReview));
 
 module.exports= router;
  
